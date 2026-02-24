0bf178352e8fd4f1aa43020da65aa71f4decba25
zeino <>
2026-02-24 01:52:56 +0000

    Tuple indexing

---
+++ mastering_python_tuples/tuple_indexing/main.py
@@ -0,0 +1,16 @@
+drama_movies = (
+    'The Shawshank Redemption',
+    'Forrest Gump',
+    'The Godfather',
+    'A Beautiful Mind',
+    'Fight Club',
+    'The Green Mile',
+    'The Pursuit of Happyness',
+    'Schindler\'s List'
+)
+
+# Write your code here
+fight_club_index = drama_movies.index('Fight Club')
+
+# Testing
+print("The 'Fight Club' movie is at index:", fight_club_index)
