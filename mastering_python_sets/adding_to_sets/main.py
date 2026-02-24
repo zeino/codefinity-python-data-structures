173272c24db987e23e82b480cd30fa01bf7fb037
zeino <>
2026-02-24 02:01:03 +0000

    Adding to Sets

---
+++ mastering_python_sets/adding_to_sets/main.py
@@ -0,0 +1,14 @@
+tarantino_movies = {
+    'Pulp Fiction',
+    'Kill Bill: Vol. 1',
+    'Kill Bill: Vol. 2',
+    'Inglourious Basterds',
+    'Django Unchained'
+}
+
+# Write your code here
+tarantino_movies.add("Reservoir Dogs")
+tarantino_movies.add("Once Upon a Time in Hollywood")
+
+# Testing
+print("Updated set:", tarantino_movies)
