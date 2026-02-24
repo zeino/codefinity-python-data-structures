ae992cd78066fa6dd8429d010cfbdd4fa7bdad26
zeino <>
2026-02-24 01:41:25 +0000

    Tuple addition

---
+++ mastering_python_tuples/tuple_addition/main.py
@@ -0,0 +1,7 @@
+animal_movies = ('The Lion King', 'Jurassic Park', 'Finding Nemo')
+
+# Write your code here
+add_movies = ('Dumbo', 'Zootopia')
+animal_movies += add_movies
+
+print("Updated animal movies:", animal_movies)
