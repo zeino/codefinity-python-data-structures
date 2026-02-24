32d4ab374886390fb825c71f0755f7333796ca89
zeino <>
2026-02-24 02:02:08 +0000

    Updating Sets

---
+++ mastering_python_sets/updating_sets/main.py
@@ -0,0 +1,13 @@
+marvel_movies = {
+    'Avengers: Endgame',
+    'Black Panther',
+    'Iron Man'
+}
+
+movies_to_add = ('Spider-Man: No Way Home', 'Guardians of the Galaxy')
+
+# Write your code here
+marvel_movies.update(movies_to_add)
+
+# Testing
+print("Updated set:", marvel_movies)
