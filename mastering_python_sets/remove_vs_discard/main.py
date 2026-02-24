c33c001261cdf39039695a1e1cdcd8832838a84b
zeino <>
2026-02-24 02:07:46 +0000

    remove vs discard

---
+++ mastering_python_sets/remove_vs_discard/main.py
@@ -0,0 +1,17 @@
+marvel_movies = {
+    'Avengers: Endgame',
+    'Black Panther',
+    'Iron Man',
+    'The Dark Knight',
+    'Spider-Man: No Way Home',
+    'Guardians of the Galaxy',
+    'Justice League'
+}
+
+# Write your code here
+marvel_movies.discard("The Dark Knight")
+
+marvel_movies.discard("Justice League")
+
+# Testing
+print("Updated set:", marvel_movies)
